2e694ee03d9577ba83a8381d46b639e2fab60637
Jared Stromberg <jaredstromberg@gmail.com>
2020-08-13 16:47:00 -0600

    Successfully implemented the topographical surface plot with projected contours.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs
     return rows.map(function(row) { return row[key]; });
   }
 
-  let z_data = []
+  let z_data = [];
   for(i=0;i<24;i++) {
     z_data.push(unpack(rows, i));
   }
@@ -29,4 +29,55 @@ Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs
   };
   Plotly.newPlot(topographicalSurfacePlot, data, layout);
 })
+
+Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv', function(err, rows) {
+  function unpack(rows, key) {
+    return rows.map(function(row) {
+      return row[key];
+    });
+  }
+
+  let z_data = [];
+  for(i=0;i<24;i++) {
+    z_data.push(unpack(rows,i));
+  }
+
+  let data = [{
+    z: z_data,
+    type: 'surface',
+    contours: {
+      z: {
+        show: true,
+        usecolormap: true,
+        highlightcolor: "#42f462",
+        project: {
+          z: true
+        }
+      }
+    }
+  }];
+
+  let layout = {
+    title: 'Mt Bruno Elevation With Projected Contours',
+    scene: {
+      camera: {
+        eye: {
+          x: 1.87,
+          y: 0.88,
+          z: -0.64
+        }
+      }
+    },
+    autosize: false,
+    margin: {
+      l: 65,
+      r: 50,
+      b: 65,
+      t: 90,
+    }
+  };
+
+  Plotly.newPlot(topographicalSurfacePlotWithContours, data, layout)
+})
+
 
