7613c00d990542d681d06bb61922c5caffead34e
Jared Stromberg <jaredstromberg@gmail.com>
2020-08-14 14:18:31 -0600

    Finalized implementation of multiple surface plots, with randomized data populated for the upper and lower surface plots.

--- app.js
+++ app.js
@@ -102,3 +102,40 @@ z1 = [
 ];
 
 z2 = [];
+for (let i = 0; i < z1.length; i++) {
+  z2_row = [];
+  for (let j = 0; j < z1[i].length; j++) {
+    z2_row.push(z1[i][j] + 1 + Math.random());
+  }
+  z2.push(z2_row);
+}
+
+z3 = [];
+for (let i = 0; i < z1.length; i++) {
+  z3_row = [];
+  for (let j = 0; j < z1[i].length; j++) {
+    z3_row.push(z1[i][j] - 1 - Math.random());
+  }
+  z3.push(z3_row);
+}
+
+let data_z1 = {
+  z: z1, 
+  type: 'surface'
+};
+
+let data_z2 = {
+  z: z2,
+  showscale: false,
+  opacity: 0.9,
+  type: 'surface'
+};
+
+let data_z3 = {
+  z: z3,
+  showscale: false,
+  opacity: 0.9,
+  type: 'surface'
+};
+
+Plotly.newPlot(multipleTopographicalPlots, [data_z1, data_z2, data_z3]);
